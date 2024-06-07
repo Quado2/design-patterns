1881f5f9117e538b2649397da7b7fdf810f0d383
Quado2 <engchikwas404@gmail.com>
2024-06-07 18:03:37 +0100

    Implement proxy pattern

---
+++ src/proxy/ATM.java
@@ -0,0 +1,18 @@
+package proxy;
+
+public class ATM implements Account { // This is proxy to bank account object not real
+    BankAccount bankAccount;
+    @Override
+    public void withdraw() {
+        //Access using actual object. You can also have checks on withdraw to achieve authentication or pin is correct or not.
+        if(bankAccount == null) {
+            bankAccount = new BankAccount();
+        }
+        bankAccount.withdraw();
+    }
+
+    @Override
+    public void getAccountNumber() {
+
+    }
+}
